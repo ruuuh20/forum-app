6cf104935db97a02f4958cb3bdd4bb54cd16c5ea
ruuuh20 <pkim2320@gmail.com>
2018-05-02 14:28:00 -0400

    add view files and crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
 
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+
   def index
   end
 
@@ -8,7 +11,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
 
@@ -22,9 +25,30 @@ class PostsController < ApplicationController
 
   end
 
+  def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
   private
 
   def post_params
 		params.require(:post).permit(:title, :content)
 	end
+
+  def find_post
+		@post = Post.find(params[:id])
+	end
+
 end
